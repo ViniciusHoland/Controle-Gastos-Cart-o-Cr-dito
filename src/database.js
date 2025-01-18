e1e37914803830254137c4cd1b5e461d7b24b28a
ViniciusDev <viniciusdev10@gmail.com>
2025-01-18 09:00:46 -0300

    update variable mongo_uri

---
+++ src/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Conexão com MongoDB realizada com sucesso!');
+    } catch (err) {
+        console.error('Erro ao conectar ao MongoDB:', err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
+
